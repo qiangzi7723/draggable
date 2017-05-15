4d34767e76c18510c77e92ba96339ff03e0ff4aa
qq1092672755@gmail.com <qq1092672755@gmail.com>
2017-05-15 16:38:01 +0800

    update

--- src/draggable.js
+++ src/draggable.js
@@ -1,9 +1,4 @@
 (function(window) {
-    // 加入AMD
-    // 需要开放的接口
-    // 事件的绑定
-    // 加上右键检测
-    // input是否添加？
     var Util = function() {
 
     }
@@ -15,6 +10,7 @@
 
     Util.prototype.hackRequestAnimationFrame = function(arguments) {
         window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+        // requestAnimationFrame的回退
         if (!window.requestAnimationFrame) {
             var lastTime = 0;
             window.requestAnimationFrame = function(callback) {
@@ -28,6 +24,7 @@
     }
 
     Util.prototype.hackTransform = function() {
+        // 兼容transform和WebkitTransform
         var docElem = document.documentElement;
         this.transformProperty = typeof docElem.style.transform == 'string' ?
             'transform' : 'WebkitTransform';
@@ -38,6 +35,7 @@
     }
 
     Util.prototype.hackStyle = function(elem) {
+        // 兼容IE8的样式获取
         if (window.getComputedStyle) {
             return window.getComputedStyle(elem);
         } else {
@@ -52,11 +50,31 @@
         this.init();
     }
     // 为了兼容IE8 只能放弃使用Object.create()实现继承
-    // var proto = Draggable.prototype = Object.create(Util.prototype);
     var proto = Draggable.prototype = new Util();
+
+    proto.init = function() {
+        var context = this;
+        this.setTargetDom();
+        this.hackEventListener();
+        if (!this.eventListener) this.options.backToPosition = true;
+        if (this.checkIsTouch()) {
+            // 说明是手机端 手机端的事件还需要兼容更多手机
+            this.elem.addEventListener('touchstart', this);
+        } else {
+            if (this.eventListener) {
+                this.elem.addEventListener('mousedown', this);
+            } else {
+                this.bindAttach();
+                // 为了兼容IE8 优雅的代码全部需要改写
+                this.elem.attachEvent('onmousedown', this.b_mousedown);
+            }
+        }
+        if (!this.options.cursorCancel) this.elem.style.cursor = 'move';
+    }
     proto.setTargetDom = function() {
         this.elem = this.getDom(this.options.elemString);
         this.parentMove = this.getDom(this.options.parentMove);
+        // 如果参数使用了parentMove接口，那么就使用parentMove作为拖拽的目标元素
         this.targetDom = this.parentMove || this.elem;
     }
     proto.getDom = function(elem) {
@@ -64,6 +82,121 @@
             return document.querySelector(elem);
         } else {
             return elem;
+        }
+    }
+    proto.bindAttach = function() {
+        // bindAttach只是为了方便IE8下的事件绑定而写的一个方法 可以省略
+        var context = this;
+        var type = ['mousedown', 'mousemove', 'mouseup'];
+        for (var i = 0, len = type.length; i < len; i++) {
+            this['b_' + type[i]] = (function(i) {
+                return function() {
+                    context.event = window.event;
+                    context[type[i]]();
+                }
+            }(i))
+        }
+    }
+    proto.dragDown = function(event) {
+        this.enable = true;
+        this.hackTransform();
+        this.addClassName();
+        this.setIndex();
+        this.style = this.hackStyle(this.targetDom);
+        this.targetPosition = this.getPosition(this.style);
+        this.startPoint = this.getCoordinate();
+        this.movePoint = {
+            x: 0,
+            y: 0
+        };
+        this.setPositionProperty();
+        this.bindCallBackEvent();
+        this.render();
+    }
+    proto.addClassName=function(){
+        if (this.options.addClassName) this.elem.className += ' ' + this.options.addClassName;
+    }
+    proto.setIndex=function(){
+        // this.elem.style.zIndex=2147483647;
+    }
+    // 需要把transform上设置的值暂时转换到使用position属性实现
+    // 这是为了方便拖拽过程中拖拽动画的实现 如果看的不太明白可以先行略过
+    proto.getPosition = function(style) {
+        var position = {};
+        position.x = style.left == 'auto' ? 0 : parseInt(style.left, 10);
+        position.y = style.top == 'auto' ? 0 : parseInt(style.top, 10);
+        // 如果设置了backToPosition属性，那么我们就不需要修改初始的transform属性，直接跳出函数
+        if (this.options.backToPosition) return position;
+        position = this.addTransform(position);
+        return position;
+    }
+    proto.addTransform = function(position) {
+        var transform = this.style[this.transformProperty];
+        if (!transform || transform.indexOf('matrix') == '-1') {
+            // 如果当前元素没有设置transform属性，那么我们可以直接返回position
+            return position;
+        }
+        // 如果是2D的transform，那么translate的值的索引以4开始，否则就是3D，以12开始
+        var translateIndex = transform.indexOf('matrix3d') == '-1' ? 4 : 12;
+        var transformArray = transform.split(',');
+        this.translateX = parseInt(transformArray[translateIndex], 10);
+        this.translateY = parseInt(transformArray[translateIndex + 1], 10);
+        position.x += this.translateX;
+        position.y += this.translateY;
+        return position;
+    }
+    // 获取鼠标的坐标
+    proto.getCoordinate = function() {
+        if (this.eventListener) {
+            return {
+                // 最后的0是为了避免当 this.event.pageX==0 的时候会取 touches[0] 的值
+                x: this.event.pageX || (this.event.touches && this.event.touches[0].pageX) || 0,
+                y: this.event.pageY || (this.event.touches && this.event.touches[0].pageY) || 0
+            }
+        } else {
+            return {
+                // 兼容IE8的鼠标位置获取
+                x: this.event.clientX + document.documentElement.scrollLeft,
+                y: this.event.clientY + document.documentElement.scrollTop
+            }
+        }
+    }
+    proto.setPositionProperty = function() {
+        var p = {
+            fix: true,
+            absolute: true,
+            relative: true
+        };
+        if (!p[this.style.position]) {
+            this.targetDom.style.position = 'relative';
+        }
+        this.targetDom.style.cssText+=';'+'left:'+this.targetPosition.x + 'px;top:'+this.targetPosition.y + 'px;';
+    }
+    // 绑定之后的事件 比如mousemove和mouseup
+    proto.bindCallBackEvent = function() {
+        var context = this;
+        var type = this.event.type;
+        var handleObj = {
+            mousedown: ['mousemove', 'mouseup'],
+            touchstart: ['touchmove', 'touchend']
+        }
+        var handles = handleObj[type];
+        this.handles = handles;
+        // true绑定事件 false解绑事件
+        this.bindEvent(true);
+    }
+    proto.bindEvent = function(isBind) {
+        var context = this;
+        var handles = this.handles;
+        if (this.eventListener) {
+            var eventListener = isBind ? 'addEventListener' : 'removeEventListener';
+            handles.forEach(function(handle) {
+                window[eventListener](handle, context);
+            })
+        } else {
+            var eventListener = isBind ? 'attachEvent' : 'detachEvent';
+            document[eventListener]('onmousemove', this.b_mousemove);
+            document[eventListener]('onmouseup', this.b_mouseup);
         }
     }
     proto.render = function() {
@@ -83,137 +216,10 @@
         if (!this.options.backToPosition) {
             this.targetDom.style[this.transformProperty] = 'translate3d(' + this.movePoint.x + 'px,' + this.movePoint.y + 'px,' + '0)';
         } else {
-            var cssString = 'left:' + (this.movePoint.x + this.elemPosition.x) + 'px;top:' + (this.movePoint.y + this.elemPosition.y) + 'px;';
+            var cssString = 'left:' + (this.movePoint.x + this.targetPosition.x) + 'px;top:' + (this.movePoint.y + this.targetPosition.y) + 'px;';
             // cssText会覆盖原样式 所以需要写+ 另外;是为了兼容IE8的cssText不返回; 不加上会出BUG
             this.targetDom.style.cssText += ';' + cssString;
         }
-    }
-    // 事件绑定只做两种 分别是mousemove touchmove
-    proto.init = function() {
-        v = this;
-        var context = this;
-        this.setTargetDom();
-        this.hackEventListener();
-        if (!this.eventListener) this.options.backToPosition = true;
-        if (this.checkIsTouch()) {
-            // 说明是手机端
-            this.elem.addEventListener('touchstart', this);
-        } else {
-            if (this.eventListener) {
-                this.elem.addEventListener('mousedown', this);
-            } else {
-                this.bindAttach();
-                // 为了兼容IE8 优雅的代码全部需要改写
-                this.elem.attachEvent('onmousedown', this.b_mousedown);
-            }
-        }
-        if (!this.options.cursorCancel) this.elem.style.cursor = 'move';
-    }
-    proto.bindAttach = function() {
-        var context = this;
-        var type = ['mousedown', 'mousemove', 'mouseup'];
-        for (var i = 0, len = type.length; i < len; i++) {
-            this['b_' + type[i]] = (function(i) {
-                return function() {
-                    context.event = window.event;
-                    context[type[i]]();
-                }
-            }(i))
-        }
-    }
-    proto.dragDown = function(event) {
-        this.enable = true;
-        this.hackTransform();
-        this.addClassName();
-        this.setIndex();
-        this.style = this.hackStyle(this.targetDom);
-        this.elemPosition = this.getPosition(this.style);
-        this.startPoint = this.getCoordinate();
-        this.movePoint = {
-            x: 0,
-            y: 0
-        };
-        this.setPositionProperty();
-        this.bindCallBackEvent();
-        this.render();
-    }
-    proto.setIndex=function(){
-        // this.elem.style.zIndex=2147483647;
-    }
-    proto.addClassName=function(){
-        if (this.options.addClassName) this.elem.className += ' ' + this.options.addClassName;
-    }
-    // 绑定之后的事件 比如mousemove和mouseup
-    proto.bindCallBackEvent = function() {
-        var context = this;
-        var type = this.event.type;
-        var handleObj = {
-            mousedown: ['mousemove', 'mouseup'],
-            touchstart: ['touchmove', 'touchend']
-        }
-        var handles = handleObj[type];
-        this.handles = handles;
-        // true绑定事件 false解绑事件
-        this.bindEvent(true);
-    }
-    // 绑定以及解绑mousemove mouseup
-    proto.bindEvent = function(isBind) {
-        var context = this;
-        var handles = this.handles;
-        if (this.eventListener) {
-            var eventListener = isBind ? 'addEventListener' : 'removeEventListener';
-            handles.forEach(function(handle) {
-                window[eventListener](handle, context);
-            })
-        } else {
-            var eventListener = isBind ? 'attachEvent' : 'detachEvent';
-            document[eventListener]('onmousemove', this.b_mousemove);
-            document[eventListener]('onmouseup', this.b_mouseup);
-        }
-
-    }
-    proto.setPositionProperty = function() {
-        var p = {
-            fix: true,
-            absolute: true,
-            relative: true
-        };
-        if (!p[this.style.position]) {
-            this.targetDom.style.position = 'relative';
-        }
-        this.targetDom.style.left = this.elemPosition.x + 'px';
-        this.targetDom.style.top = this.elemPosition.y + 'px';
-    }
-    // getPosition除了获取元素的position值之外 还需要考虑transform的影响
-    proto.getPosition = function(style) {
-        var position = {};
-        position.x = style.left == 'auto' ? 0 : parseInt(style.left, 10);
-        position.y = style.top == 'auto' ? 0 : parseInt(style.top, 10);
-        if (this.options.backToPosition) {
-            // 说明开启了回退position，所以不需要叠加transform属性
-            return position;
-        }
-        // 把初始的transform值放到position中
-        position = this.addTransform(position);
-        return position;
-    }
-    proto.addTransform = function(position) {
-        // 在IE8下返回null
-        var transform = this.style[this.transformProperty];
-        if (!transform || transform.indexOf('matrix') == '-1') {
-            // 说明transform属性不存在
-            return position;
-        }
-        // 如果是2D的transform，那么translate的值的索引以4开始，否则就是3d，以12开始
-        var translateIndex = transform.indexOf('matrix3d') == '-1' ? 4 : 12;
-        var transformArray = transform.split(',');
-        // 拖拽结束的时候还需要手动的还原transform的值
-        this.translateX = parseInt(transformArray[translateIndex], 10);
-        // 获取到的值可能是100)这样的情况，带了最后的括号
-        this.translateY = parseInt(transformArray[translateIndex + 1], 10);
-        position.x += this.translateX;
-        position.y += this.translateY;
-        return position;
     }
     proto.dragMove = function() {
         var vector = this.getCoordinate();
@@ -222,7 +228,6 @@
             y: vector.y - this.startPoint.y
         }
         moveVector = this.setGrid(moveVector);
-        // 这里每次move的时候都需要进行两次的三木判断 性能上是否有一定的影响
         this.movePoint.x = this.options.axis == 'y' ? 0 : moveVector.x;
         this.movePoint.y = this.options.axis == 'x' ? 0 : moveVector.y;
     }
@@ -234,7 +239,6 @@
         vector.y = grid.y ? Math.round(moveVector.y / grid.y) * grid.y : moveVector.y;
         return vector;
     }
-    // 解绑事件 设置position 清除定时器
     proto.dragUp = function() {
         var context = this;
         this.enable = false;
@@ -255,28 +259,12 @@
     }
     proto.resetPosition = function() {
         this.endPoint = {
-            x: this.movePoint.x + this.elemPosition.x,
-            y: this.movePoint.y + this.elemPosition.y
+            x: this.movePoint.x + this.targetPosition.x,
+            y: this.movePoint.y + this.targetPosition.y
         }
         this.targetDom.style.cssText+=';left:'+this.endPoint.x + 'px;top:'+this.endPoint.y + 'px;transform:translate3d(0,0,0)';
     }
-    // 获取坐标
-    proto.getCoordinate = function() {
-        if (this.eventListener) {
-            return {
-                // 最后的0是为了避免当 this.event.pageX==0 的时候会取 touches[0] 的值
-                x: this.event.pageX || (this.event.touches && this.event.touches[0].pageX) || 0,
-                y: this.event.pageY || (this.event.touches && this.event.touches[0].pageY) || 0
-            }
-        } else {
-            return {
-                // 兼容IE8的鼠标位置获取
-                x: this.event.clientX + document.documentElement.scrollLeft,
-                y: this.event.clientY + document.documentElement.scrollTop
-            }
-        }
 
-    }
     proto.touchstart = function(event) {
         this.dragDown(event);
     }
